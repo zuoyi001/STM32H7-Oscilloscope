58df118bb8539e9118b8445d870965e2b04cad6e
niu14789 <164100086@qq.com>
2020-03-13 21:49:10 +0800

    增加LED闪烁进程

---
+++ Oscilloscope/dev/led.c
@@ -0,0 +1,120 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "fos.h"
+#include "main.h"
+#include "led.h"
+/* LED define */
+#define LED_RED_GPIO GPIOB
+#define LED_RED_GPIO_PIN GPIO_PIN_9
+#define LED_GREEN_GPIO GPIOH
+#define LED_GREEN_GPIO_PIN GPIO_PIN_5
+/* register node */
+FOS_INODE_REGISTER("led",led_hardware_init,0,0,10);
+/* define task run as 500ms */
+FOS_TSK_REGISTER(led_thread_500ms,PRIORITY_4,500);
+FOS_TSK_REGISTER(led_thread_300ms,PRIORITY_4,100);
+/* ------------------------ */
+int led_hardware_init(void)
+{
+		/* default dev */
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+	/* enable clock */
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+	__HAL_RCC_GPIOH_CLK_ENABLE();
+	/* display */
+  GPIO_InitStruct.Pin = LED_RED_GPIO_PIN;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_PULLUP;
+	GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
+	/* init */
+	HAL_GPIO_Init(LED_RED_GPIO, &GPIO_InitStruct);	
+	/* ohers */
+	GPIO_InitStruct.Pin = LED_GREEN_GPIO_PIN;
+	/* init */
+	HAL_GPIO_Init(LED_GREEN_GPIO, &GPIO_InitStruct);	
+	/* led on */
+	HAL_GPIO_WritePin(LED_RED_GPIO,LED_RED_GPIO_PIN,GPIO_PIN_RESET);	
+	HAL_GPIO_WritePin(LED_GREEN_GPIO,LED_GREEN_GPIO_PIN,GPIO_PIN_RESET);
+	/* return */
+	return FS_OK;
+}
+/* led thread run as 500ms */
+static void led_thread_500ms(void)
+{
+	HAL_GPIO_TogglePin(LED_RED_GPIO,LED_RED_GPIO_PIN);
+}
+/* led thread run as 300ms */
+static void led_thread_300ms(void)
+{
+	/* freq ctrl */
+	static unsigned int led_freq_ctrl = 0;
+	static unsigned char flag_led = 0;
+	/* freq ctrl */
+	if( led_freq_ctrl > 48 )
+	{
+		/* test */
+		if( flag_led == 1 )
+		{
+			/* set flags */
+			flag_led = 0;
+			/* on the led */			
+			HAL_GPIO_WritePin(LED_GREEN_GPIO,LED_GREEN_GPIO_PIN,GPIO_PIN_RESET);
+		}
+	}
+	else
+	{
+		/* test */
+		if( flag_led == 0 )
+		{
+			/* set flags */
+			flag_led = 1;
+			/* off the led */
+			HAL_GPIO_WritePin(LED_GREEN_GPIO,LED_GREEN_GPIO_PIN,GPIO_PIN_SET);
+		}
+	}
+	/* clear */
+	if( led_freq_ctrl++ >= 50 )
+	{
+		led_freq_ctrl = 0;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Oscilloscope/dev/led.h
@@ -0,0 +1,29 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : notify.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+	* BEEP TIM3 CHANNEL1 PWM Gerente
+	* LED is TIM4 CH3 and CH4
+  */
+#ifndef __LED_H__
+#define __LED_H__	
+
+/* Includes ------------------------------------------------------------------*/
+static int led_hardware_init(void);
+static void led_thread_500ms(void);
+static void led_thread_300ms(void);
+
+#endif
